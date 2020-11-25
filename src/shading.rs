3e96bb84ca0be679daf542a60c8dbe0e9d6992ef
ifsheldon <maplevantablack@outlook.com>
2020-11-25 22:27:50 +0800

    added comment for easy understanding

--- src/shading.rs
+++ src/shading.rs
@@ -161,6 +161,7 @@ pub fn look_at(eye: &Vec3, center: &Vec3, up: &Vec3) -> Mat3 {
 
 #[inline]
 pub fn ray_direction_perspective(fov_radian: f32, frag_coord: &[f32; 2]) -> Vec3 {
+    // ignore the case when the aspect of view != aspect of window
     let x = frag_coord[0] - WIDTH_HF + 0.5;
     let y = frag_coord[1] - HEIGHT_HF + 0.5;
     let z = HEIGHT_F / (fov_radian / 2.).tan();
